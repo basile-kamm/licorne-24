ba0a1e7988361cc115b0032c5ca9fe4b448bc30f
basile-kamm <another-black-kitty@proton.me>
2024-06-18 11:10:27 +0200

    hover random color

--- src/js/main.js
+++ src/js/main.js
@@ -10,3 +10,40 @@ document.querySelector("main").addEventListener("click", function () {
     burger.classList.remove("is-open");
   }
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const colors = [
+    "#749fd5",
+    "#65194f",
+    "#ea8d13",
+    "#673a8f",
+    "#edd811",
+    "#52b293",
+  ];
+
+  function applyRandomColorOnHover(elements, property) {
+    elements.forEach((element) => {
+      let previousColor = null;
+
+      element.addEventListener("mouseenter", function () {
+        let randomColor;
+
+        do {
+          randomColor = colors[Math.floor(Math.random() * colors.length)];
+        } while (randomColor === previousColor);
+
+        previousColor = randomColor;
+        element.style.setProperty(property, randomColor);
+      });
+    });
+  }
+
+  const buttonElements = document.querySelectorAll(".button");
+  applyRandomColorOnHover(buttonElements, "--random-color");
+
+  const svgElements = document.querySelectorAll("svg .svg-g");
+  applyRandomColorOnHover(svgElements, "--random-color");
+
+  const submitElement = document.querySelector(".news-submit");
+  applyRandomColorOnHover([submitElement], "--random-color");
+});
